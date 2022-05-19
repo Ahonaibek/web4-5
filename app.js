896a65649f0483f2f866c70fd92a566d07952291
Ahonaibek <ahonaibek@gmail.com>
2022-05-19 15:07:29 +0600

    2345

--- app.js
+++ app.js
@@ -95,4 +95,8 @@ app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'public', 'index.ejs'));
 });
 
+app.listen((process.env.PORT || 3000), function(){
+    console.log('listening on *:3000');
+});
+
 module.exports = app;
